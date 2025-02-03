17bb2c33771edf58ce601b2f81ba7a1bca6c2fbf
Kristina Garina <kristina.garina@intapp.com>
2025-02-03 17:27:49 +0200

    setup report

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,28 +5,27 @@ export default defineConfig({
   fullyParallel: false,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : 1,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+
+  outputDir: '.artifacts/test-results',
+
+  reporter: [['html', { outputFolder: '.artifacts//html-report' }]],
+
   use: {
     baseURL: 'https://www.saucedemo.com/',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    // outputDir: 'my-reports/test-results',
   },
+
   projects: [
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
-
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
